e794f0aa066fc9c066d78fc48969a78bc8f26d45
TonyLi0916 <tonyli4505@gmail.com>
2025-09-12 22:52:08 -0400

    solutions

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newWord = '';
+    newWord = word.split('').reverse().join('');
+    return newWord;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(num) {
+    return Math.round((num - 32) * (5/9) * 1e1) / 1e1; 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(num) {
+  return Math.round((num * (9/5) + 32) * 1e1 ) / 1e1;
 };
+
 
 // Do not edit below this line
 module.exports = {
